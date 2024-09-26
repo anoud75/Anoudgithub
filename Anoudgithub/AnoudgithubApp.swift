ef67535000e399c0dd499fc59e4c6b46499e320e
Alanoud <Alanoudsaud75@gmail.com>
2024-09-26 16:53:39 +0300

    Initial Commit

---
+++ Anoudgithub/AnoudgithubApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnoudgithubApp.swift
+//  Anoudgithub
+//
+//  Created by Alanoud Alsamil  on 23/03/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct AnoudgithubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AnoudgithubUITests/AnoudgithubUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  AnoudgithubUITestsLaunchTests.swift
+//  AnoudgithubUITests
+//
+//  Created by Alanoud Alsamil  on 23/03/1446 AH.
+//
+
+import XCTest
+
+final class AnoudgithubUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
